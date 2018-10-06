ce57bcd3b6a2813ab197550c0a820edfcb7d85ad
nyaachen <nyaachen@github.com>
2018-10-06 08:40:51 +0800

    初始定义-课程
    对课程对象的初始定义

---
+++ lab01/courses.h
@@ -0,0 +1,29 @@
+#pragma once
+#ifndef COURSES_H
+#define COURSES_H
+
+#include <string>
+#include <vector>
+
+namespace xuanke {
+	enum Weekday {MON,TUE,WED,THU,FRI};
+	enum Hour {MORNING1, MORNING2, AFTERNOON1};
+	class Time {
+	private:
+		Weekday day;
+		Hour hr;
+	public:
+		Time(Weekday d, Hour h);
+		bool operator==(const Time &t);
+		bool operator<(const Time &t);
+	};
+	class Courses {
+	private:
+		std::string Name;
+		std::string Department;
+		std::vector<Time> Timetable;
+	public:
+		Courses(std::string name, std::string department, std::vector<Time> timetable);
+	};
+}
+#endif
